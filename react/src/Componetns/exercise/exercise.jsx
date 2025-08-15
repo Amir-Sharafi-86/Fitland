7ac098da6ff00b47bea27c099a1ed31c696760e9
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-08-16 02:03:35 +0330

    coding section exercise details

--- react/src/Componetns/exercise/exercise.jsx
+++ react/src/Componetns/exercise/exercise.jsx
@@ -1,9 +1,103 @@
 import React from 'react'
 import ExerciseDetails from '../exerciseDetails/exerciseDetails'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 function Exercise() {
     return (
         <>
-         <ExerciseDetails></ExerciseDetails>
+         <h1 className='text-base  md:text-[32px] text-[#021526] text-center mb-8 font-MorabbaBold'>تخفیفات ویژه</h1>
+          <div className='container mx-auto'>
+            <div className='relative hidden md:flex gap-x-2.5 bottom-4.5  '>  
+            <button 
+      className='cursor-pointer  rounded-full mb-8 p-2 swiper-button-next absolute top-1/2 right-0 w-10 h-10 -translate-y-1/2 z-20'
+      type="button"
+      aria-label="Next Slide"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+      </svg>
+    </button>
+    
+    
+    
+              <button 
+                className='cursor-pointer  rounded-full mb-8 p-2 swiper-button-prev absolute top-2/2  size-10 -translate-y-1/2 z-20'
+                type="button"
+                aria-label="Previous Slide"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
+                </svg>
+              </button>
+            </div>
+    
+            <Swiper
+              modules={[Autoplay, Navigation]}
+              spaceBetween={20}
+              slidesPerView={2}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              navigation={{
+                nextEl: ".swiper-button-prev",
+                prevEl: ".swiper-button-next",
+              }}
+              breakpoints={{
+                768: { slidesPerView: 2 },
+              }}
+            >
+    
+
+                {/* <SwiperSlide>
+        <ExerciseDetails
+          title="کفش نایک مدل S-2000"
+          price="۲۰۰۰۰۰ تومان"
+          size="دوچرخه مدل AO-14"
+          img="/public/images/main/cickle2.jpg"
+          delay="200"
+          duration="800"
+      
+        />
+      </SwiperSlide> */}
+             
+              <SwiperSlide>
+                <ExerciseDetails
+                  title="دوچرخه مدل S-2000"
+                  price="۳۰۰,۰۰۰ تومان"
+                  size="از سایز L تا XXL"
+                  img="/public/images/main/cickle.jpg"
+                  isTrue = "true"
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ExerciseDetails
+                  title="اسکوتر مدل A-155"
+                  price="۳۰۰,۰۰۰ تومان"
+                  size="از سایز L تا XXL"
+                  img="/public/images/main/sck.jpg"
+                  delay="0"
+                  duration="800"
+                  
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ExerciseDetails
+                  title="اسکوتر مدل A-155"
+                  price="۳۰۰,۰۰۰ تومان"
+                  size="از سایز L تا XXL"
+                  img="/public/images/main/sck.jpg"
+                  delay="0"
+                  duration="800"
+                  
+                />
+              </SwiperSlide>
+             
+    
+            </Swiper>
+          </div>
         </>
     )
 }
--- react/src/Componetns/exerciseDetails/exerciseDetails.jsx
+++ react/src/Componetns/exerciseDetails/exerciseDetails.jsx
@@ -4,8 +4,9 @@ function ExerciseDetails() {
     return (
        <>
 
-          <div className="main-box__content mb-20 bg-white">
-              <img src="/images/main/exersice.png"  className='rounded-md' alt="" />
+          <div className="main-box__content mb-20  border border-[#CBCBCB] bg-white">
+              <img src="/images/main/exersice.png"  className='rounded-md w-full' alt="" />
+              <span className=''>افزایش دریافتی فوتبالیست ها و معافیت از سربازی </span>
           </div>
         
        </>
