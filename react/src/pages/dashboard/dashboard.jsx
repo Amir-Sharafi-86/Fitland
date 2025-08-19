3b188afd0e7f0ebbb6e8fc29f0ad944129bbba61
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-08-19 21:45:13 +0330

    start section dashboard

--- react/src/pages/dashboard/dashboard.jsx
+++ react/src/pages/dashboard/dashboard.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Header from '../../Componetns/Header/header'
 
 function Dashboard() {
     return (
-        <div>
-            <h1>cwjcwpcpji</h1>
-        </div>
+        <>
+            <Header></Header>
+
+            <main className='max-w-[1440px]'>
+                <div className="container">
+                    <div className="grid grid-cols-12">
+                        <div className='sidebar  col-span-3'>
+
+                        </div>
+                        //!Content User 
+                        <div className='col-span-9'></div>
+                    </div>
+                </div>
+            </main>
+
+        </>
     )
 }
 
