4b75ac11757ff738bf93b5a186795b2655072930
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-08-20 23:30:07 +0330

    start section coding login

---
+++ react/src/pages/login/login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Login() {
+    return (
+            <>
+                <main className='max-w-[1440px] mx-auto '>
+                    <div className="container h-screen flex justify-center items-center">
+                        <div className="form__infos">
+                            <h1 className='text-[#000000] text-3xl'>ورود | ثبت‌نام</h1>
+                        </div>
+                       <form action="">
+
+                       </form>
+                    </div>
+                </main>
+            </>        
+    )
+}
+
+export default Login
--- react/src/routes.jsx
+++ react/src/routes.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import Index from './pages/Index';
 import Dashboard from './pages/dashboard/dashboard';
 import Product from './pages/product/Product';
-
+import Login from './pages/login/login';
 const routes = [
   { path: '/', element: <Index /> },
   { path: '/dashboard', element: <Dashboard /> },
   { path: '/Product', element: <Product /> },
+  { path: '/login', element: <Login /> },
 ];
 
 export default routes;
