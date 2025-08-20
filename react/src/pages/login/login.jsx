940040e5c9b9b1b416a88368d0b68b76df28fc11
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-08-20 23:45:54 +0330

    final coding form

--- react/src/pages/login/login.jsx
+++ react/src/pages/login/login.jsx
@@ -5,12 +5,15 @@ function Login() {
             <>
                 <main className='max-w-[1440px] mx-auto '>
                     <div className="container h-screen flex justify-center items-center">
-                        <div className="form__infos">
-                            <h1 className='text-[#000000] text-3xl'>ورود | ثبت‌نام</h1>
-                        </div>
-                       <form action="">
+                        <div className="form bg-white w-[456px]  border border-[#EDEDED] p-8.5">
+                            <h1 className='text-[#000000] text-[28px] mb-8 font-MorabbaMedium'>ورود | ثبت‌نام</h1>
 
-                       </form>
+                            <form action="">
+                                <input type="text" placeholder='*********09 ' className='placeholder:text-[#ADADAD] py-2 px-3 placeholder:text-sm  placeholder:font-MorabbaMedium border border-[#ADADAD] w-full rounded-lg  outline-none text-black  focus:outline-none focus:border focus:border-orange-noraml' />
+                            </form>
+
+                            <button className='btn-orange text-white font-MorabbaBold'>ادامه</button>
+                        </div>
                     </div>
                 </main>
             </>        
