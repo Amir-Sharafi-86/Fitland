0ef1766a2d5a9c808ba21a3827e0b6be811d1684
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-09-15 21:15:31 +0330

    solve error for step deployment

--- fitland-next/src/app/cart/page.tsx
+++ fitland-next/src/app/cart/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import Header from '@/Components/Header/header'
 
-interface Props {}
 
-function Page(props: Props) {
-  const {} = props
+function Page() {
 
   return (
     <>
