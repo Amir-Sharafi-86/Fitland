459fdf9bbd657e36895d91a41cdf7c66b22b65e2
Amir Sharafi <Amirsharafi8899@gmail.com>
2025-11-25 14:46:21 +0330

    edit navbar

--- fitland-next/src/Components/Nav-menu/navMenu.tsx
+++ fitland-next/src/Components/Nav-menu/navMenu.tsx
@@ -15,7 +15,7 @@ function NavMenu({ title }: NavMenuProps) {
           <ul className="text-[#000306] text-xs transition-colors lg:text-base font-MorabbaBold flex justify-between bg-[#F9F9F9] py-4 px-10 rounded-2xl">
             
             {/* Left Menu */}
-            <div className="left-menu flex gap-x-5 lg:gap-x-10">
+            <div className="left-menu flex gap-x-5 xl:gap-x-10">
                      <Link href={"/product"} className="hover:text-orange-noraml">مردانه</Link>
                      <Link href={"/product"} className="hover:text-orange-noraml">زنانه</Link>
                      <li className="relative menu___item ml">
@@ -26,7 +26,7 @@ function NavMenu({ title }: NavMenuProps) {
 
             </div>
             {/* Right Menu */}
-            <div className="right-menu flex gap-x-5 lg:gap-x-10">
+            <div className="right-menu flex gap-x-5 xl:gap-x-10">
               <li className="flex gap-x-1 hover:text-orange-noraml">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-orange-noraml">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
